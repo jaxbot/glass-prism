0a8a5b8419d2c96067a4a945dce62513de205bba
Jonathan Warner <jaxbot@gmail.com>
2014-03-05 22:30:38 -0500

    Tighten up the file layout

--- index.js
+++ index.js
@@ -16,170 +16,47 @@ var mirror = null;
 
 // dot templates for the cards
 var dot = require('dot');
-var cards = {};
-
-// load all cards and turn into templates
-var files = fs.readdirSync("cards/");
-for (var i = 0; i < files.length; i++) {
-	cards[files[i].replace('.html','')] = dot.template(fs.readFileSync("cards/"+files[i]))
-}
+exports.cards = {};
 
 // user card storage
-var user_card_ids = {};
 var client_tokens = [];
 
-var sessionhash = Math.random() * 1e8;
+var verify_hash = null;
 
-// read the connected users information from disk
-try {
-	var filedata = fs.readFileSync(".clienttokens.json");
-	if (filedata) {
-		client_tokens = JSON.parse(filedata.toString());
-		oauth2Client.credentials = client_tokens[0];
+
+exports.init = function(config, callback) {
+	// load all cards and turn into templates
+	var files = fs.readdirSync("cards/");
+	for (var i = 0; i < files.length; i++) {
+		exports.cards[files[i].replace('.html','')] = dot.template(fs.readFileSync("cards/"+files[i]))
 	}
-} catch(e) {
-	console.log("Info: failed to load .clienttokens.json, using blank array");
-}
+	
+	// read the connected users information from disk
+	try {
+		var filedata = fs.readFileSync(".clienttokens.json");
+		if (filedata) {
+			client_tokens = JSON.parse(filedata.toString());
+			oauth2Client.credentials = client_tokens[0];
+		}
+	} catch(e) {
+		console.log("Info: failed to load .clienttokens.json, using blank array");
+	}
 
-exports.init = function(config) {
 	oauth2Client = new googleapis.OAuth2Client(config.client_id, config.client_secret, config.redirect_dir);
 
+	verify_hash = config.verify_hash;
+
 	googleapis.discover('mirror','v1').execute(function(err,client) {
-		if (err) {
-			console.warn("ERR: " + err.toString());
-			return;
-		}
+		if (err) return callback(err);
+
 		mirror = client.mirror;
 
 		http.createServer(httpHandler).listen(config.port);
+
+		callback(null);
 	});
 }
 
-	
-function httpHandler(req,res) {
-	var u = url.parse(req.url, true)
-	var s = u.pathname.split("/");
-	var page = s[s.length-1];
-
-	if (page == "subscription") {
-		var postBody = "";
-		req.on("data",function(data) {
-			postBody += data;
-			if (postBody.length > 1024 * 1024) {
-				postBody = null;
-				req.end();
-			}
-		});
-		req.on("end", function(data) {
-			try {
-				var d = JSON.parse(postBody);
-				console.log(d);
-
-				/*
-				if (d.verifyToken != sessionhash) {
-					console.log("Bad hash!");
-					res.end();
-					return;
-				}
-				*/
-
-				if (!client_tokens[d.userToken]) {
-					console.log("Bad user token");
-					res.end();
-					return;
-				}
-				oauth2Client.credentials = client_tokens[d.userToken];
-				if (d.itemId) {
-					apiclient.mirror.timeline.get({
-						id: d.itemId
-					}).withAuthClient(oauth2Client).execute(function(err,data) {
-						console.log(err);
-						console.log(data);
-
-						//data.text
-					});
-				}
-				res.end(200);
-			} catch (e) {
-				res.end();
-			}
-		});
-	}
-	
-	if (page == "oauth2callback") {
-		oauth2Client.getToken(u.query.code, function(err,tokens) {
-			if (err) {
-				console.log(err);
-				res.writeHead(500);
-				res.write("Uh oh: The token login failed. Chances are you loaded a page that was already loaded. Try going back and pressing the 'get it on glass' button again.");
-				res.end();
-			} else {
-				var index = client_tokens.push(tokens) - 1;
-
-				fs.writeFile(".clienttokens.json", JSON.stringify(client_tokens,null,5));
-
-				client_tokens.push(tokens);
-
-				getSystemLoadInfo();
-
-				oauth2Client.credentials = tokens;
-
-				// add subscriptions
-				apiclient.mirror.subscriptions.insert({
-					"callbackUrl": config.subscription_callback,
-					"collection": "timeline",
-					"operation": [], // empty set = all
-					"userToken": index,
-					"verifyToken": sessionhash
-				}).withAuthClient(oauth2Client).execute(function(err,data) {
-					if (err) {
-						console.log(err);
-					}
-				});
-
-				// add contact interface
-				apiclient.mirror.contacts.insert({
-					"id": "gtop_contact_provider_"+config.source_id,
-					"displayName": "gtop: " + config.hostname,
-					"speakableName": config.speakableName,
-					"imageUrls": [config.contactIcon],
-					"priority": 7,
-					"acceptCommands": [
-						{"type":"POST_AN_UPDATE"}
-					]
-				}).withAuthClient(oauth2Client).execute(function(err,data) {
-					if (err)
-						console.log(err);
-				});
-
-				res.writeHead(302, { "Location": "success" });
-				res.end();
-			}
-		});
-		return;
-	}
-
-	if (page == "success") {
-		res.writeHead(200, { 'Content-type': 'text/html' });
-		fs.createReadStream("pages/success.html").pipe(res);
-		return;
-	}
-	
-	if (page == "authorize") {
-		var uri = oauth2Client.generateAuthUrl({
-			access_type: 'offline',
-			approval_prompt: 'force',
-			scope: 'https://www.googleapis.com/auth/glass.timeline'
-		});
-		res.writeHead(302, { "Location": uri });
-		res.end();
-		return;
-	}
-	
-	// nothing else, so just show default
-	res.writeHead(200, { 'Content-type': 'text/html' });
-	fs.createReadStream("pages/index.html").pipe(res);
-};
 
 /* updateAllCards
  * Update cards for all users by id
@@ -225,4 +102,122 @@ exports.mirrorCall = function(call, tokens, callback) {
 	oauth2Client.credentials = tokens;
 	call.withAuthClient(oauth2Client).execute(callback);
 }
+	
+function httpHandler(req,res) {
+	var parsedUrl = url.parse(req.url, true)
+	var s = parsedUrl.pathname.split("/");
+	var page = s[s.length-1];
+
+	switch (page) {
+		case "subscription":
+			var postBody = "";
+			req.on("data",function(data) {
+				postBody += data;
+				if (postBody.length > 1024 * 1024) {
+					postBody = null;
+					req.end();
+				}
+			});
+			req.on("end", function(data) {
+				try {
+					var d = JSON.parse(postBody);
+					console.log(d);
+
+					if (d.verifyToken != verify_hash) {
+						console.log("Bad hash!");
+						res.end();
+						return;
+					}
+
+					if (!client_tokens[d.userToken]) {
+						console.log("Bad user token");
+						res.end();
+						return;
+					}
+					oauth2Client.credentials = client_tokens[d.userToken];
+					if (d.itemId) {
+						apiclient.mirror.timeline.get({
+							id: d.itemId
+						}).withAuthClient(oauth2Client).execute(function(err,data) {
+							if (err) return subscriptionCallback(err);
+
+							subscriptionCallback(err, { data: d, item: data });
+						});
+					}
+					res.end(200);
+				} catch (e) {
+					res.end(500);
+				}
+			});
+			break;
+		case "oauth2callback":
+			oauth2Client.getToken(u.query.code, function(err,tokens) {
+				if (err) {
+					console.log(err);
+					res.writeHead(500);
+					res.write("Uh oh: The token login failed. Chances are you loaded a page that was already loaded. Try going back and pressing the 'get it on glass' button again.");
+					res.end();
+				} else {
+					var index = client_tokens.push(tokens) - 1;
+
+					fs.writeFile(".clienttokens.json", JSON.stringify(client_tokens,null,5));
+
+					client_tokens.push(tokens);
+
+					getSystemLoadInfo();
+
+					oauth2Client.credentials = tokens;
+
+					// add subscriptions
+					apiclient.mirror.subscriptions.insert({
+						"callbackUrl": config.subscription_callback,
+						"collection": "timeline",
+						"operation": [], // empty set = all
+						"userToken": index,
+						"verifyToken": verify_hash
+					}).withAuthClient(oauth2Client).execute(function(err,data) {
+						if (err) {
+							console.log(err);
+						}
+					});
+
+					// add contact interface
+					apiclient.mirror.contacts.insert({
+						"id": "gtop_contact_provider_"+config.source_id,
+						"displayName": "gtop: " + config.hostname,
+						"speakableName": config.speakableName,
+						"imageUrls": [config.contactIcon],
+						"priority": 7,
+						"acceptCommands": [
+							{"type":"POST_AN_UPDATE"}
+						]
+					}).withAuthClient(oauth2Client).execute(function(err,data) {
+						if (err)
+							console.log(err);
+					});
+
+					res.writeHead(302, { "Location": "success" });
+					res.end();
+				}
+			});
+			break;
+		case "success":
+			res.writeHead(200, { 'Content-type': 'text/html' });
+			fs.createReadStream("pages/success.html").pipe(res);
+			break;
+		case "authorize":
+			var uri = oauth2Client.generateAuthUrl({
+				access_type: 'offline',
+				approval_prompt: 'force',
+				scope: 'https://www.googleapis.com/auth/glass.timeline'
+			});
+			res.writeHead(302, { "Location": uri });
+			res.end();
+			break;
+		default:
+			res.writeHead(200, { 'Content-type': 'text/html' });
+			fs.createReadStream("pages/index.html").pipe(res);
+			break;
+	}
+};
 
